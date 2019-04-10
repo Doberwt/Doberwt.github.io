88c3a5c71dd6828884a54b8f28fed164cce3e21f
Doberwt <48659312+Doberwt@users.noreply.github.com>
2019-04-10 22:05:59 +0200

    Update geo.js

--- geo.js
+++ geo.js
@@ -19,16 +19,6 @@ function showPosition(o) {
     })
 }
 
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
-    }
-  };
-  xhttp.open("GET", "https://doberwt.github.io/zend", true);
-  xhttp.send();
-}
 
 function loadDoc() {
   var xhttp = new XMLHttpRequest();
